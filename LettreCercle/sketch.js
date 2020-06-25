6fa7e2a5ebe62633631bb2c12621664089d28456
Stephane <stephane.conreaux@yahoo.fr>
2020-06-25 15:11:45 +0200

    adding LettreCercle

---
+++ LettreCercle/sketch.js
@@ -0,0 +1,99 @@
+let font = null;
+const circles = [];
+const initialRadius = 2;
+let canvas = null;
+let graphic = null;
+const spots = [];
+
+function preload() {
+  font = loadFont("./LEMONMILK-Regular.otf");
+}
+
+function setup() {   
+  canvas = createCanvas(1000, 400);
+
+  // create the image from text
+  // and get the 'white' pixels
+  background(0);
+  textSize(350);
+  fill(255);
+  noStroke();
+  textStyle(BOLD);
+  text("2020",100,300);
+  loadPixels();
+  clear();
+  let size = 0;
+  for( let y=0; y < height; y++ ) {
+    for( let x=0; x < width; x++ ) {
+      const index = x + y*width;
+      const b = pixels[index*4]; // trick 4 indices per pixel (R G B A)
+      if( b > 250) {
+        spots.push(createVector(x,y));
+      }
+    }
+  }
+} 
+
+function addNewCircle() {
+  const spotIndex = Math.floor(random(spots.length));
+  const x = spots[spotIndex].x;
+  const y = spots[spotIndex].y;
+  let valid = true;
+  circles.forEach(c=>{
+    const d = dist(x,y,c.location.x,c.location.y);
+    if( d < c.r + initialRadius ) {
+      valid = false;
+    }
+  });
+  if( valid ) {
+    return new Circle(x, y, initialRadius);
+  } else {
+    return null;
+  }
+}
+
+function draw() { 
+  background(51);
+
+  const total = 1;
+  let count = 0;
+  let attempts = 0;
+
+  while( count < total ) {
+    const c = addNewCircle();
+    if( c !== null ) {
+      circles.push(c);
+      count++;
+    }
+    attempts++;
+    if( attempts > 1000 ) {
+      noLoop();
+      console.log("rendered");
+      break;
+    }
+  }
+
+  for( const circle of circles ) {
+    if( circle.growing ) {
+      if( circle.edges() ){
+        circle.growing = false;
+        break;
+      } else {
+        for( const other of circles ) {
+          if( other !== circle ) {
+            const d = dist(circle.location.x,circle.location.y,other.location.x,other.location.y);
+            if( d < circle.r + other.r + 2) {
+              circle.growing = false;
+              break;
+            }
+          }
+        }
+      }
+    }
+    circle.grow();
+  }
+
+  for( const circle of circles ) {
+    circle.show();
+  }
+}
